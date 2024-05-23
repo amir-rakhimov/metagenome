e56fc1a8cbe9e295046719095229d6a080ff3a4d
amir-rakhimov <rakhimovamir2001@gmail.com>
2024-05-23 20:10:24 +0900

    save the output of HUMANn3 and gzip

--- code/bash-scripts/humann3-pipeline.sh
+++ code/bash-scripts/humann3-pipeline.sh
@@ -3,13 +3,16 @@
 ### Remove the hyphen with sed (use global search with /g to remove all hyphens)
 ### Store as a variable date_var
 date_var=$(date -I|sed 's/-//g')
-nthreads=10
+time_var=$(date +%T |sed 's/:/_/g' )
+date_time=${date_var}_${time_var}
+nthreads=8
+project_home_dir=~/projects/metagenome
 ### directory with decontaminated fastq files
 bowtie2_decontam_fastq_dir=data/bowtie2_decontam_fastq
 # mkdir output/humann3_pipeline
 humann_out_dir=output/humann3_pipeline
 # mkdir data/humann3_db
-humann3_db_dir=data/humann3_db/humann3_full_20240208
+humann3_db_dir=data/humann3_db/humann3_full_20240513
 source ~/miniconda3/etc/profile.d/conda.sh
 # # Set conda channel priority
 # conda config --add channels defaults
@@ -29,6 +32,11 @@ conda activate humann-tools-3.7
 # to update the default location of this database. Alternatively you can always provide 
 # the location of the chocophlan database you would like to use with the
 #  "--nucleotide-database " option to humann.
+# humann_config --update database_folders protein $DIR 
+# humann_config --update database_folders protein \
+	# /lustre7/home/rakhimov/projects/metagenome/data/humann3_db/humann3_full_20240513/uniref
+# humann_config --update database_folders nucleotide \
+	# /lustre7/home/rakhimov/projects/metagenome/data/humann3_db/humann3_full_20240513/chocophlan
 
 ## Download a translated search database
 ## To download the full UniRef90 database (20.7GB, recommended)
@@ -57,9 +65,9 @@ conda activate humann-tools-3.7
 #  SAMPLE=$(echo ${FILE} | sed "s/_decontam_R1\.fastq\.gz//")
 #  base_name=$(basename "$SAMPLE" )
 #  zcat ${bowtie2_decontam_fastq_dir}/${base_name}_decontam_R1.fastq.gz |head -n 40| \
-#  gzip -1 --fast > ${bowtie2_decontam_fastq_dir}/${base_name}_subset_R1.fastq.gz
+#  gzip -9 --best > ${bowtie2_decontam_fastq_dir}/${base_name}_subset_R1.fastq.gz
 #  zcat ${bowtie2_decontam_fastq_dir}/${base_name}_decontam_R2.fastq.gz |head -n 40| \
-#  gzip -1 --fast > ${bowtie2_decontam_fastq_dir}/${base_name}_subset_R2.fastq.gz;
+#  gzip -9 --best > ${bowtie2_decontam_fastq_dir}/${base_name}_subset_R2.fastq.gz;
 # done
 # # Merge subset data
 # for FILE in ${bowtie2_decontam_fastq_dir}/*subset_R1.fastq.gz
@@ -68,36 +76,87 @@ conda activate humann-tools-3.7
 #  base_name=$(basename "$SAMPLE" )
 #  zcat ${bowtie2_decontam_fastq_dir}/${base_name}_subset_R1.fastq.gz \
 #  ${bowtie2_decontam_fastq_dir}/${base_name}_subset_R2.fastq.gz | \
-#  gzip -1 --fast > ${bowtie2_decontam_fastq_dir}/${base_name}_subset_merged.fastq.gz;
+#  gzip -9 --best > ${bowtie2_decontam_fastq_dir}/${base_name}_subset_merged.fastq.gz;
 # done
 
 # Full data
 # Need to merge paired end reads by concatenation
-#for FILE in ${bowtie2_decontam_fastq_dir}/*decontam_R1.fastq.gz
-#do 
-# SAMPLE=$(echo ${FILE} | sed "s/_decontam_R1\.fastq\.gz//")
-# base_name=$(basename "$SAMPLE" )
-# zcat ${bowtie2_decontam_fastq_dir}/${base_name}_decontam_R1.fastq.gz \
-# ${bowtie2_decontam_fastq_dir}/${base_name}_decontam_R2.fastq.gz | \
-# gzip -1 --fast > ${bowtie2_decontam_fastq_dir}/${base_name}_merged.fastq.gz;
-#done
+# for FILE in ${bowtie2_decontam_fastq_dir}/*decontam_R1.fastq.gz
+# do 
+#  SAMPLE=$(echo ${FILE} | sed "s/_decontam_R1\.fastq\.gz//")
+#  base_name=$(basename "$SAMPLE" )
+#  zcat ${bowtie2_decontam_fastq_dir}/${base_name}_decontam_R1.fastq.gz \
+#   ${bowtie2_decontam_fastq_dir}/${base_name}_decontam_R2.fastq.gz | \
+#   gzip -9 --best > ${bowtie2_decontam_fastq_dir}/${base_name}_merged.fastq.gz;
+# done
 
 # Humann
+### We skip prescreen with Metaphlan because our community is not well-characterised. 
+### We avoid loss of data.
+### Humann will create a custom ChocoPhlan database, run bowtie2-build, bowtie2,
+### then perform nucleotide alignment, and align reads to uniref90 with diamond
+# for FILE in ${bowtie2_decontam_fastq_dir}/*merged.fastq.gz
+# do 
+#  SAMPLE=$(echo ${FILE} | sed "s/_merged\.fastq\.gz//")
+#  base_name=$(basename "$SAMPLE" )
+#  humann  --threads ${nthreads} \
+# 	-i ${bowtie2_decontam_fastq_dir}/${base_name}_merged.fastq.gz \
+# 	-o ${humann_out_dir}/humann_out_${date_time}\
+# 	--nucleotide-database ${humann3_db_dir}/chocophlan \
+#     --protein-database ${humann3_db_dir}/uniref \
+# 	--bypass-prescreen
+# #  rm ${bowtie2_decontam_fastq_dir}/${base_name}_merged.fastq.gz
+# #  cp ${humann_out_dir}/humann_out_${date_time}/${base_name}_merged_humann_temp/${base_name}_merged_metaphlan_bugs_list.tsv  ${humann_out_dir}/humann_out_${date_time}/
+#  cp ${humann_out_dir}/humann_out_${date_time}/${base_name}_merged_humann_temp/${base_name}_merged.log  ${humann_out_dir}/humann_out_${date_time}/
+#  rm -rf ${humann_out_dir}/humann_out_${date_time}/${base_name}_merged_humann_temp;
+# done
+
+# humann -i data/bowtie2_decontam_fastq/2D10_subset_merged.fastq.gz \
+# 	-o output/humann3_pipeline/humann_out_${date_time}\
+# 	--protein-database data/humann3_db/humann3_full_20240513/uniref \
+# 	--bypass-prescreen
+
+# 	--nucleotide-database data/humann3_db/humann3_full_20240513/chocophlan \
+# We can skip creation of custom Chocophlan database if you already have it using
+#  --nucleotide-database $OUTPUT_DIR/$SAMPLE_1_humann_temp/ --bypass-nucleotide-index
+# (I run out of memory during bowtie2 build-index and I don't want to create the database 
+# again
 for FILE in ${bowtie2_decontam_fastq_dir}/*merged.fastq.gz
 do 
  SAMPLE=$(echo ${FILE} | sed "s/_merged\.fastq\.gz//")
  base_name=$(basename "$SAMPLE" )
  humann  --threads ${nthreads} \
 	-i ${bowtie2_decontam_fastq_dir}/${base_name}_merged.fastq.gz \
-	-o ${humann_out_dir}/humann_out_${date_var}\
+	-o ${humann_out_dir}/humann_out_${date_time}\
 	--nucleotide-database ${humann3_db_dir}/chocophlan \
-    --protein-database ${humann3_db_dir}/uniref
- rm ${bowtie2_decontam_fastq_dir}/${base_name}_merged.fastq.gz
- cp ${humann_out_dir}/humann_out_${date_var}/${base_name}_merged_humann_temp/${base_name}_merged_metaphlan_bugs_list.tsv  ${humann_out_dir}/humann_out_${date_var}/
- cp ${humann_out_dir}/humann_out_${date_var}/${base_name}_merged_humann_temp/${base_name}_merged.log  ${humann_out_dir}/humann_out_${date_var}/
- rm -rf ${humann_out_dir}/humann_out_${date_var}/${base_name}_merged_humann_temp;
+	--protein-database ${humann3_db_dir}/uniref \
+	--bypass-prescreen 
+ cp ${humann_out_dir}/humann_out_${date_time}/${base_name}_merged_humann_temp/${base_name}_merged.log  ${humann_out_dir}/humann_out_${date_time}
+ cd ${humann_out_dir}/humann_out_${date_time}/
+ rm ${base_name}_merged_humann_temp/*bt2l
+ rm ${base_name}_merged_humann_temp/${base_name}_merged_custom_chocophlan_database.ffn
+ cd ${project_home_dir}/${humann_out_dir}/humann_out_${date_time}
+ tar -zcvf ${base_name}_merged_humann_temp.tar ${base_name}_merged_humann_temp/.
+ gzip -9 --best ${base_name}_merged_humann_temp.tar
+ cd ${project_home_dir};
 done
 
+# SAMPLE is data/bowtie2_decontam_fastq/2D14_trim
+# base_name is 2D14_trim
+
+# for FILE in ${bowtie2_decontam_fastq_dir}/*merged.fastq.gz
+# do 
+#  SAMPLE=$(echo ${FILE} | sed "s/_merged\.fastq\.gz//")
+#  base_name=$(basename "$SAMPLE" )
+#  humann  --threads ${nthreads} \
+# 	-i ${bowtie2_decontam_fastq_dir}/${base_name}_merged.fastq.gz \
+# 	-o ${humann_out_dir}/humann_out_${date_time}\
+#     --protein-database ${humann3_db_dir}/uniref \
+# 	--bypass-prescreen \
+# 	--nucleotide-database ${humann_out_dir}/humann_out_20240516_17_20_40/2D10_trim_merged_humann_temp/ \
+# 	--bypass-nucleotide-index
+#  cp ${humann_out_dir}/humann_out_${date_time}/${base_name}_merged_humann_temp/${base_name}_merged.log  ${humann_out_dir}/humann_out_${date_time};
+# done
 # humann  --threads ${nthreads} \
 # 	-i ${bowtie2_decontam_fastq_dir}/2D10_trim_subset_merged.fastq.gz \
 # 	-o ${humann_out_dir}/humann_out_trial
@@ -117,36 +176,36 @@ done
 # SRS014472-Buccal_mucosa_pathabundance.tsv # main output is in general output directory
 # SRS014459-Stool_pathcoverage.tsv # main output is in general output directory
 # SRS014472-Buccal_mucosa_pathcoverage.tsv # main output is in general output directory
-#mkdir ${humann_out_dir}/humann_merged_${date_var}
-#humann_merged_out_dir=${humann_out_dir}/humann_merged_${date_var}
+#mkdir ${humann_out_dir}/humann_merged_${date_time}
+#humann_merged_out_dir=${humann_out_dir}/humann_merged_${date_time}
 ### Let's join gene family abundance outputs into a single table
-#humann_join_tables -i ${humann_out_dir}/humann_out_${date_var} \
-#  -o ${humann_merged_out_dir}/${date_var}_humann_out_genefamilies.tsv \
+#humann_join_tables -i ${humann_out_dir}/humann_out_${date_time} \
+#  -o ${humann_merged_out_dir}/${date_time}_humann_out_genefamilies.tsv \
 #  --file_name genefamilies
 ### Notice how we don't specify each sample but the name of directory 
 ### in front of _genefamilies.tsv
 ### file_name is also just genefamilies: only join tables with this string included in the file name
-### Result: ${humann_out_dir}/humann_merged_${date_var}/${date_var}_humann_out_genefamilies.tsv
-#humann_join_tables -i ${humann_out_dir}/humann_out_${date_var} \
-#  -o ${humann_merged_out_dir}/${date_var}_humann_out_pathabundance.tsv \
+### Result: ${humann_out_dir}/humann_merged_${date_time}/${date_time}_humann_out_genefamilies.tsv
+#humann_join_tables -i ${humann_out_dir}/humann_out_${date_time} \
+#  -o ${humann_merged_out_dir}/${date_time}_humann_out_pathabundance.tsv \
 #  --file_name pathabundance
 
 # Normalizing RPKs to relative abundance
-#humann_renorm_table --input ${humann_merged_out_dir}/${date_var}_humann_out_genefamilies.tsv \
-#    --output ${humann_merged_out_dir}/${date_var}_humann_out_genefamilies-cpm.tsv \
+#humann_renorm_table --input ${humann_merged_out_dir}/${date_time}_humann_out_genefamilies.tsv \
+#    --output ${humann_merged_out_dir}/${date_time}_humann_out_genefamilies-cpm.tsv \
 #	--units cpm \
 #	--update-snames
 # for pathabundance renorm is enough
-#humann_renorm_table --input ${humann_merged_out_dir}/${date_var}_humann_out_pathabundance.tsv \
-#    --output ${humann_merged_out_dir}/${date_var}_humann_out_pathabundance-cpm.tsv \
+#humann_renorm_table --input ${humann_merged_out_dir}/${date_time}_humann_out_pathabundance.tsv \
+#    --output ${humann_merged_out_dir}/${date_time}_humann_out_pathabundance-cpm.tsv \
 #	--units cpm \
 #	--update-snames
 # Regrouping genes to other functional categories
-#humann_regroup_table --input ${humann_merged_out_dir}/${date_var}_humann_out_genefamilies-cpm.tsv \
-#    --output ${humann_merged_out_dir}/${date_var}_humann_out_genefamilies-cpm-rxn.tsv \
+#humann_regroup_table --input ${humann_merged_out_dir}/${date_time}_humann_out_genefamilies-cpm.tsv \
+#    --output ${humann_merged_out_dir}/${date_time}_humann_out_genefamilies-cpm-rxn.tsv \
 #	--groups uniref90_rxn
 
 # Attaching names to features
-#humann_rename_table --input ${humann_merged_out_dir}/${date_var}_humann_out_genefamilies-cpm-rxn.tsv \
-#    --output ${humann_merged_out_dir}/${date_var}_humann_out_genefamilies-cpm-rxn-named.tsv \
+#humann_rename_table --input ${humann_merged_out_dir}/${date_time}_humann_out_genefamilies-cpm-rxn.tsv \
+#    --output ${humann_merged_out_dir}/${date_time}_humann_out_genefamilies-cpm-rxn-named.tsv \
 #	--names metacyc-rxn
