3491fae2f45c9cc894496c9fb137aa86108c803f
amir-rakhimov <rakhimovamir2001@gmail.com>
2024-06-28 11:25:50 +0900

    added bbmap to qc-tools

--- code/bash-scripts/qc-create.sh
+++ code/bash-scripts/qc-create.sh
@@ -5,6 +5,6 @@ conda config --add channels bioconda
 conda config --add channels conda-forge
 conda config --add channels biobakery
 source ~/miniconda3/etc/profile.d/conda.sh
-#conda create -y --name qc-tools -c bioconda bowtie2 cutadapt samtools trf fastqc multiqc seqtk
+#conda create -yqn qc-tools -c bioconda bowtie2 cutadapt samtools trf fastqc multiqc seqtk bbmap
 conda activate qc-tools
 conda install -y cutadapt
