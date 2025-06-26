9c52bcd30aae8b0bab8a857503bcd35a967d0fb6
amir-rakhimov <rakhimovamir2001@gmail.com>
2025-06-26 17:40:27 +0900

    added a separate classify command because classify_wf crashes

--- code/bash-scripts/mag-assembly/004-taxonomic-classification.sh
+++ code/bash-scripts/mag-assembly/004-taxonomic-classification.sh
@@ -3,9 +3,9 @@
 #SBATCH -N 4
 #SBATCH -n 40
 #SBATCH --mem-per-cpu 8g
-#SBATCH -J 20250515_11-06-mag_assembly
-#SBATCH --output jobreports/20250515_11-06-mag_assembly-megahit-metabat2-pipeline-out.txt
-#SBATCH --error jobreports/20250515_11-06-mag_assembly-megahit-metabat2-pipeline-out.txt
+#SBATCH -J 20250619_19-13-004-taxonomic-classification
+#SBATCH --output jobreports/20250619_19-13-004-taxonomic-classification-out.txt
+#SBATCH --error jobreports/20250619_19-13-004-taxonomic-classification-out.txt
 #I am requesting 4 nodes containing 40 CPUs, with 8 GB memory per CPU. Total: 320 GB. 280 GB for GTDB-TK
 source ~/miniconda3/etc/profile.d/conda.sh
 shopt -s nullglob
@@ -23,55 +23,25 @@ shopt -s nullglob
 # Metaeuk annotates contigs to find eukaryotic genes
 date_var=$(date -I|sed 's/-//g')
 time_var=$(date +%T |sed 's/:/_/g' )
-date_time=${date_var}_${time_var}
+date_time="${date_var}_${time_var}"
 start_date_time=$(date +"%F %H:%M:%S")
-# For binning with MetaBAT2
-megahit_date_time=20250303_17_54_26
-metaquast_megahit_date_time=20250303_17_54_26
-prodigal_date_time=20250303_17_54_26
-bbwrap_date_time=20250326_08_18_26
-metabat2_date_time=20250417_23_21_29
-metaquast_metabat2_date_time=20250417_23_21_29
-busco_date_time=20250417_23_21_29
-checkm2_date_time=20250501_07_53_55
-gtdbtk_date_time=20250514_17_57_12
-prokka_date_time="${date_time}"
+# metabat2_date_time=20250417_23_21_29
+metabat2_date_time=20250612_13_37_47
+drep_date_time="${date_time}"
+# gtdbtk_date_time=20250514_17_57_12
+gtdbtk_date_time="${date_time}"
 nthreads=40
 nthreads_sort=35
 mem_req=8G
 mem_req_sort=4G
 project_home_dir=~/projects/metagenome
 output_dir=~/projects/metagenome/output/mag_assembly
-bowtie2_decontam_fastq_dir=data/bowtie2_decontam_fastq
-megahit_output_dir=output/mag_assembly/megahit_output
-megahit_aligned_reads_dir=output/mag_assembly/megahit_output/alignedreads
-contig_coverages_dir=output/mag_assembly/contig_coverages
-prodigal_output_dir=output/mag_assembly/prodigal_output
-metaquast_output_dir=output/mag_assembly/metaquast_output
-metaquast_script_dir=~/quast-5.2.0
-bam_contig_depths_dir=output/mag_assembly/bam_contig_depths
 metabat2_output_dir=output/mag_assembly/metabat2_output
-metabat2_reports_dir=output/mag_assembly/metabat2_reports
-samtools_reports_dir=output/mag_assembly/samtools_reports
-checkm2_output_dir=output/mag_assembly/checkm2_output
+drep_output_dir=output/mag_assembly/drep_output
 gtdbtk_output_dir=output/mag_assembly/gtdbtk_output
-bbwrap_refs_dir=output/mag_assembly/bbwrap_refs
-busco_output_dir=output/mag_assembly/busco_output
-prokka_output_dir=output/mag_assembly/prokka_output
 
 cd ${project_home_dir}
-mkdir -p output/mag_assembly/megahit_output/alignedreads
-mkdir -p output/mag_assembly/metabat2_reports
-mkdir -p output/mag_assembly/samtools_reports
-mkdir -p output/mag_assembly/checkm2_output
 mkdir -p output/mag_assembly/gtdbtk_output
-mkdir -p output/mag_assembly/bam_contig_depths
-mkdir -p output/mag_assembly/bbwrap_refs
-mkdir -p output/mag_assembly/metaquast_output
-mkdir -p output/mag_assembly/busco_output
-mkdir -p output/mag_assembly/prodigal_output
-mkdir -p output/mag_assembly/prokka_output
-mkdir -p output/mag_assembly/contig_coverages
 
 # 0. Show the current time for logging
 echo "${start_date_time}"
@@ -79,13 +49,10 @@ echo "${start_date_time}"
 # 1. Activate conda environment
 conda activate mag_assembly-tools
 
-
 # 7. Classify with GTDB-TK
 # GTDB-Tk requires ~110G of external data that needs to be downloaded and unarchived
 intermediate_date_time=$(date +"%F %H:%M:%S")
 echo "${intermediate_date_time}"
-conda deactivate
-conda activate mag_assembly-tools
 # download-db.sh
 # wget https://data.ace.uq.edu.au/public/gtdb/data/releases/latest/auxillary_files/gtdbtk_package/full_package/gtdbtk_data.tar.gz \
 #  -O ~/miniconda3/envs/mag_assembly-tools/share/gtdbtk-2.4.0/db/gtdbtk_data.tar.gz
@@ -93,28 +60,90 @@ conda activate mag_assembly-tools
 echo "Running GTDB-TK"
 # Fix pplacer locale error
 export LC_ALL=C
-# for FILE_DIR in "${metabat2_output_dir}"/"${metabat2_date_time}"*bins
-# do 
-#  SAMPLE=$(echo "${FILE_DIR}" | sed "s/${metabat2_date_time}_//" |sed "s/_bins//")
-#  base_name=$(basename "$SAMPLE" )
-#  echo "Running GTDB-TK on ${base_name}"
-#  gtdbtk classify_wf --genome_dir "${metabat2_output_dir}"/"${metabat2_date_time}"_"${base_name}"_bins/ \
-#     --out_dir "${gtdbtk_output_dir}"/"${gtdbtk_date_time}"_"${base_name}"_GTDBtk \
-#     -x fa \
-#     --skip_ani_screen \
-#     --cpus "${nthreads_sort}";
-# done
+for FILE_DIR in "${metabat2_output_dir}"/"${metabat2_date_time}"*bins
+do 
+ SAMPLE=$(echo "${FILE_DIR}" | sed "s/${metabat2_date_time}_//" |sed "s/_bins//")
+ base_name=$(basename "$SAMPLE" )
+ echo "Running GTDB-TK classify_wf on ${base_name}"
+ gtdbtk classify_wf --genome_dir "${metabat2_output_dir}"/"${metabat2_date_time}"_"${base_name}"_bins/ \
+    --out_dir "${gtdbtk_output_dir}"/"${gtdbtk_date_time}"_"${base_name}"_GTDBtk \
+    -x fa \
+    --skip_ani_screen \
+    --cpus "${nthreads_sort}" \
+    --pplacer_cpus 1
+ # Run classify separately because pplacer didn't recognize gz files
+ echo "Running GTDB-TK classify on ${base_name}"
+ #  gunzip "${gtdbtk_output_dir}"/"${gtdbtk_date_time}"_"${base_name}"_GTDBtk/align/*.gz
+ gtdbtk classify --genome_dir "${metabat2_output_dir}"/"${metabat2_date_time}"_"${base_name}"_bins/ \
+    --out_dir "${gtdbtk_output_dir}"/"${gtdbtk_date_time}"_"${base_name}"_GTDBtk \
+    --align_dir "${gtdbtk_output_dir}"/"${gtdbtk_date_time}"_"${base_name}"_GTDBtk \
+    -x fa \
+    --skip_ani_screen \
+    --cpus "${nthreads_sort}" \
+    --pplacer_cpus 1;
+done
 # Run classify separately because pplacer didn't recognize gz files
 for FILE_DIR in "${metabat2_output_dir}"/"${metabat2_date_time}"*bins
 do 
  SAMPLE=$(echo "${FILE_DIR}" | sed "s/${metabat2_date_time}_//" |sed "s/_bins//")
  base_name=$(basename "$SAMPLE" )
- echo "Running GTDB-TK on ${base_name}"
- gunzip "${gtdbtk_output_dir}"/"${gtdbtk_date_time}"_"${base_name}"_GTDBtk/align/*.gz
+ echo "Running GTDB-TK classify on ${base_name}"
+#  gunzip "${gtdbtk_output_dir}"/"${gtdbtk_date_time}"_"${base_name}"_GTDBtk/align/*.gz
  gtdbtk classify --genome_dir "${metabat2_output_dir}"/"${metabat2_date_time}"_"${base_name}"_bins/ \
-    --out_dir "${gtdbtk_output_dir}"/"${gtdbtk_date_time}"_"${base_name}"_GTDBtk/classify \
-    --align_dir "${gtdbtk_output_dir}"/"${gtdbtk_date_time}"_"${base_name}"_GTDBtk/align \
+    --out_dir "${gtdbtk_output_dir}"/"${gtdbtk_date_time}"_"${base_name}"_GTDBtk \
+    --align_dir "${gtdbtk_output_dir}"/"${gtdbtk_date_time}"_"${base_name}"_GTDBtk \
     -x fa \
     --skip_ani_screen \
-    --cpus "${nthreads_sort}";
+    --cpus "${nthreads_sort}" \
+    --pplacer_cpus 1;
 done
+intermediate_date_time=$(date +"%F %H:%M:%S")
+echo "${intermediate_date_time}"
+
+
+
+# # Merge GTDB-Tk data within each sample
+# for FILE_DIR in "${gtdbtk_output_dir}"/"${gtdbtk_date_time}"*_GTDBtk/classify
+# do
+#  SAMPLE=$(echo "${FILE_DIR}" | sed "s/${gtdbtk_date_time}_//" |sed "s/_GTDBtk\/classify//")
+#  base_name=$(basename "$SAMPLE" )
+
+#  awk -F'\t' '{print $0}' "${FILE_DIR}"/gtdbtk.bac120.summary.tsv > \
+#   "${gtdbtk_output_dir}"/"${gtdbtk_date_time}"_"${base_name}"_classification.tsv
+#  awk -F'\t' 'NR>1{print $0}' "${FILE_DIR}"/gtdbtk.ar53.summary.tsv >> \
+#   "${gtdbtk_output_dir}"/"${gtdbtk_date_time}"_"${base_name}"_classification.tsv;
+# done
+
+# # Merge all GTDB-Tk data from all samples into one table
+# first_file=true
+# for file in "${gtdbtk_output_dir}"/"${gtdbtk_date_time}"*_classification.tsv; do
+#   if [ "${first_file}" = true ]; then
+#     cat "${file}"         # print header + data
+#     first_file=false
+#   else
+#     tail -n +2 "${file}"   # skip header, print only data
+#   fi
+# done > "${gtdbtk_output_dir}"/"${gtdbtk_date_time}"_classification_all.tsv
+
+
+# # 1347 MAGs in total
+# awk 'NR > 1 { count++ }
+#   END {print "There are " count " MAGs in total." }
+#   '  "${gtdbtk_output_dir}"/"${gtdbtk_date_time}"_classification_all.tsv
+
+# # Keep only high-quality dereplicated MAGs (max 126)
+# #  "${gtdbtk_output_dir}"/"${gtdbtk_date_time}"_drep_filtered_taxonomy.tsv is just a list!
+# # checkm2_date_time=20250501_07_53_55 # old binning (gtdbtk_date_time=20250514_17_57_12)
+# checkm2_date_time=20250619_05_47_09 # new binning
+# checkm2_output_dir=output/mag_assembly/checkm2_output
+
+# awk 'NR==FNR {a[$1]; next} FNR==1 || $1 in a' \
+#  "${checkm2_output_dir}"/"${checkm2_date_time}"_drep_selected_bins.txt \
+#  "${gtdbtk_output_dir}"/"${gtdbtk_date_time}"_classification_all.tsv \
+#  > "${gtdbtk_output_dir}"/"${gtdbtk_date_time}"_drep_filtered_taxonomy.tsv
+# # For a different column, replace $1 with $2, $3, etc.
+
+# # 126 MAGs left
+# awk 'NR > 1 { count++ }
+#   END {print "There are " count " high-quality derpelicated MAGs." }
+#   '  "${gtdbtk_output_dir}"/"${gtdbtk_date_time}"_drep_filtered_taxonomy.tsv
