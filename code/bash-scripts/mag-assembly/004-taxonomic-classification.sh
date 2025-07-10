2869a2910b67cdba42c867a03d37dd715ca45ae9
amir-rakhimov <rakhimovamir2001@gmail.com>
2025-07-10 18:28:23 +0900

    Changed the GTDB-tk code to run it on dereplicated MAGs instead of all samples. Added CompareM to calculate average amino acid identity among MAGs

--- code/bash-scripts/mag-assembly/004-taxonomic-classification.sh
+++ code/bash-scripts/mag-assembly/004-taxonomic-classification.sh
@@ -3,35 +3,30 @@
 #SBATCH -N 4
 #SBATCH -n 40
 #SBATCH --mem-per-cpu 8g
-#SBATCH -J 20250619_19-13-004-taxonomic-classification
-#SBATCH --output jobreports/20250619_19-13-004-taxonomic-classification-out.txt
-#SBATCH --error jobreports/20250619_19-13-004-taxonomic-classification-out.txt
-#I am requesting 4 nodes containing 40 CPUs, with 8 GB memory per CPU. Total: 320 GB. 280 GB for GTDB-TK
+#SBATCH -J 20250707_11-27-taxonomic-classification
+#SBATCH --output jobreports/20250707_11-27-taxonomic-classification-out.txt
+#SBATCH --error jobreports/20250707_11-27-taxonomic-classification-out.txt
+#I am requesting 4 nodes containing 40 CPUs, with 8 GB memory per CPU. Total: 320 GB
 source ~/miniconda3/etc/profile.d/conda.sh
 shopt -s nullglob
 # When nullglob is enabled, if a glob pattern does not match any files,
 # it expands to nothing (an empty string) instead of returning the pattern itself.
 # So, if no matches are found, the script will skip the file
 
-# Metaquast is a metagenome assembly evaluation tool
-# MetaBAT2 is a binning software: group contigs into bins (genomes)
-# Metaquast is also used to evaluate the quality of bins
-# CheckM2 assesses the quality of bins
 # GTDB-TK assigns taxonomy to genomes
-# Prodigal is a gene prediction software
-# Prokka annotates contigs to find bacterial genes
-# Metaeuk annotates contigs to find eukaryotic genes
 date_var=$(date -I|sed 's/-//g')
 time_var=$(date +%T |sed 's/:/_/g' )
 date_time="${date_var}_${time_var}"
 start_date_time=$(date +"%F %H:%M:%S")
 # metabat2_date_time=20250417_23_21_29
-metabat2_date_time=20250612_13_37_47
-drep_date_time="${date_time}"
+# metabat2_date_time=20250612_13_37_47
+drep_date_time=20250627_19_56_42
 # gtdbtk_date_time=20250514_17_57_12
 gtdbtk_date_time="${date_time}"
+comparem_date_time="${date_time}"
 nthreads=40
-nthreads_sort=35
+pplacer_threads=30
+nthreads_sort=15
 mem_req=8G
 mem_req_sort=4G
 project_home_dir=~/projects/metagenome
@@ -39,9 +34,11 @@ output_dir=~/projects/metagenome/output/mag_assembly
 metabat2_output_dir=output/mag_assembly/metabat2_output
 drep_output_dir=output/mag_assembly/drep_output
 gtdbtk_output_dir=output/mag_assembly/gtdbtk_output
+comparem_output_dir=output/mag_assembly/comparem_output
 
 cd ${project_home_dir}
 mkdir -p output/mag_assembly/gtdbtk_output
+mkdir -p output/mag_assembly/comparem_output
 
 # 0. Show the current time for logging
 echo "${start_date_time}"
@@ -57,49 +54,83 @@ echo "${intermediate_date_time}"
 # wget https://data.ace.uq.edu.au/public/gtdb/data/releases/latest/auxillary_files/gtdbtk_package/full_package/gtdbtk_data.tar.gz \
 #  -O ~/miniconda3/envs/mag_assembly-tools/share/gtdbtk-2.4.0/db/gtdbtk_data.tar.gz
 # tar -xvf "${GTDBTK_DATA_PATH}"/*tar.gz -C "${GTDBTK_DATA_PATH}"
-echo "Running GTDB-TK"
-# Fix pplacer locale error
-export LC_ALL=C
-for FILE_DIR in "${metabat2_output_dir}"/"${metabat2_date_time}"*bins
-do 
- SAMPLE=$(echo "${FILE_DIR}" | sed "s/${metabat2_date_time}_//" |sed "s/_bins//")
- base_name=$(basename "$SAMPLE" )
- echo "Running GTDB-TK classify_wf on ${base_name}"
- gtdbtk classify_wf --genome_dir "${metabat2_output_dir}"/"${metabat2_date_time}"_"${base_name}"_bins/ \
-    --out_dir "${gtdbtk_output_dir}"/"${gtdbtk_date_time}"_"${base_name}"_GTDBtk \
-    -x fa \
-    --skip_ani_screen \
-    --cpus "${nthreads_sort}" \
-    --pplacer_cpus 1
- # Run classify separately because pplacer didn't recognize gz files
- echo "Running GTDB-TK classify on ${base_name}"
- #  gunzip "${gtdbtk_output_dir}"/"${gtdbtk_date_time}"_"${base_name}"_GTDBtk/align/*.gz
- gtdbtk classify --genome_dir "${metabat2_output_dir}"/"${metabat2_date_time}"_"${base_name}"_bins/ \
-    --out_dir "${gtdbtk_output_dir}"/"${gtdbtk_date_time}"_"${base_name}"_GTDBtk \
-    --align_dir "${gtdbtk_output_dir}"/"${gtdbtk_date_time}"_"${base_name}"_GTDBtk \
-    -x fa \
-    --skip_ani_screen \
-    --cpus "${nthreads_sort}" \
-    --pplacer_cpus 1;
-done
-# Run classify separately because pplacer didn't recognize gz files
-for FILE_DIR in "${metabat2_output_dir}"/"${metabat2_date_time}"*bins
-do 
- SAMPLE=$(echo "${FILE_DIR}" | sed "s/${metabat2_date_time}_//" |sed "s/_bins//")
- base_name=$(basename "$SAMPLE" )
- echo "Running GTDB-TK classify on ${base_name}"
-#  gunzip "${gtdbtk_output_dir}"/"${gtdbtk_date_time}"_"${base_name}"_GTDBtk/align/*.gz
- gtdbtk classify --genome_dir "${metabat2_output_dir}"/"${metabat2_date_time}"_"${base_name}"_bins/ \
-    --out_dir "${gtdbtk_output_dir}"/"${gtdbtk_date_time}"_"${base_name}"_GTDBtk \
-    --align_dir "${gtdbtk_output_dir}"/"${gtdbtk_date_time}"_"${base_name}"_GTDBtk \
-    -x fa \
-    --skip_ani_screen \
-    --cpus "${nthreads_sort}" \
-    --pplacer_cpus 1;
-done
-intermediate_date_time=$(date +"%F %H:%M:%S")
-echo "${intermediate_date_time}"
+# echo "Running GTDB-TK"
+# # Fix pplacer locale error
+# export LC_ALL=C
+# echo "Running GTDB-TK classify_wf on dereplicated genomes (95% ANI threshold)"
+# gtdbtk classify_wf \
+#     --genome_dir "${drep_output_dir}"/"${drep_date_time}"_sa_95perc/dereplicated_genomes/ \
+#     --out_dir "${gtdbtk_output_dir}"/"${gtdbtk_date_time}"_sa_95perc_GTDBtk \
+#     -x fa \
+#     --skip_ani_screen \
+#     --cpus "${nthreads_sort}" \
+#     --pplacer_cpus "${pplacer_threads}"
+#     # --scratch_dir "${gtdbtk_output_dir}"/"${gtdbtk_date_time}"_sa_95perc_GTDBtk \
+#  # Run classify separately because pplacer didn't recognize gz files
+# intermediate_date_time=$(date +"%F %H:%M:%S")
+# echo "${intermediate_date_time}"
 
+# echo "Running GTDB-TK classify on dereplicated genomes (95% ANI threshold)"
+# gunzip "${gtdbtk_output_dir}"/"${gtdbtk_date_time}"_sa_95perc_GTDBtk/align/*.gz
+# gtdbtk classify --genome_dir "${drep_output_dir}"/"${drep_date_time}"_sa_95perc/dereplicated_genomes/ \
+#     --out_dir "${gtdbtk_output_dir}"/"${gtdbtk_date_time}"_sa_95perc_GTDBtk \
+#     --align_dir "${gtdbtk_output_dir}"/"${gtdbtk_date_time}"_sa_95perc_GTDBtk \
+#     -x fa \
+#     --skip_ani_screen \
+#     --cpus "${nthreads_sort}" \
+#     --pplacer_cpus "${pplacer_threads}"
+#     # --scratch_dir "${gtdbtk_output_dir}"/"${gtdbtk_date_time}"_sa_95perc_GTDBtk \
+# intermediate_date_time=$(date +"%F %H:%M:%S")
+# echo "${intermediate_date_time}"
+
+# echo "Running GTDB-TK classify_wf on dereplicated genomes (99% ANI threshold)"
+# gtdbtk classify_wf \
+#     --genome_dir "${drep_output_dir}"/"${drep_date_time}"_sa_99perc/dereplicated_genomes/ \
+#     --out_dir "${gtdbtk_output_dir}"/"${gtdbtk_date_time}"_sa_99perc_GTDBtk \
+#     -x fa \
+#     --skip_ani_screen \
+#     --cpus "${nthreads_sort}" \
+#     --pplacer_cpus "${pplacer_threads}"
+#     # --scratch_dir "${gtdbtk_output_dir}"/"${gtdbtk_date_time}"_sa_99perc_GTDBtk \
+#  # Run classify separately because pplacer didn't recognize gz files
+# intermediate_date_time=$(date +"%F %H:%M:%S")
+# echo "${intermediate_date_time}"
+# echo "Running GTDB-TK classify on dereplicated genomes (99% ANI threshold)"
+# gunzip "${gtdbtk_output_dir}"/"${gtdbtk_date_time}"_sa_99perc_GTDBtk/align/*.gz
+# gtdbtk classify --genome_dir "${drep_output_dir}"/"${drep_date_time}"_sa_99perc/dereplicated_genomes/ \
+#     --out_dir "${gtdbtk_output_dir}"/"${gtdbtk_date_time}"_sa_99perc_GTDBtk \
+#     --align_dir "${gtdbtk_output_dir}"/"${gtdbtk_date_time}"_sa_99perc_GTDBtk \
+#     -x fa \
+#     --skip_ani_screen \
+#     --cpus "${nthreads_sort}" \
+#     --pplacer_cpus "${pplacer_threads}"
+#     # --scratch_dir "${gtdbtk_output_dir}"/"${gtdbtk_date_time}"_sa_99perc_GTDBtk \
+# intermediate_date_time=$(date +"%F %H:%M:%S")
+# echo "${intermediate_date_time}"
+
+
+# for FILE_DIR in "${metabat2_output_dir}"/"${metabat2_date_time}"*bins
+# do 
+#  SAMPLE=$(echo "${FILE_DIR}" | sed "s/${metabat2_date_time}_//" |sed "s/_bins//")
+#  base_name=$(basename "$SAMPLE" )
+#  echo "Running GTDB-TK classify_wf on ${base_name}"
+#  gtdbtk classify_wf --genome_dir "${metabat2_output_dir}"/"${metabat2_date_time}"_"${base_name}"_bins/ \
+#     --out_dir "${gtdbtk_output_dir}"/"${gtdbtk_date_time}"_"${base_name}"_GTDBtk \
+#     -x fa \
+#     --skip_ani_screen \
+#     --cpus "${nthreads_sort}" \
+#     --pplacer_cpus 1
+#  # Run classify separately because pplacer didn't recognize gz files
+#  echo "Running GTDB-TK classify on ${base_name}"
+#  gunzip "${gtdbtk_output_dir}"/"${gtdbtk_date_time}"_"${base_name}"_GTDBtk/align/*.gz
+#  gtdbtk classify --genome_dir "${metabat2_output_dir}"/"${metabat2_date_time}"_"${base_name}"_bins/ \
+#     --out_dir "${gtdbtk_output_dir}"/"${gtdbtk_date_time}"_"${base_name}"_GTDBtk \
+#     --align_dir "${gtdbtk_output_dir}"/"${gtdbtk_date_time}"_"${base_name}"_GTDBtk \
+#     -x fa \
+#     --skip_ani_screen \
+#     --cpus "${nthreads_sort}" \
+#     --pplacer_cpus 1;
+# done
 
 
 # # Merge GTDB-Tk data within each sample
@@ -124,6 +155,32 @@ echo "${intermediate_date_time}"
 #     tail -n +2 "${file}"   # skip header, print only data
 #   fi
 # done > "${gtdbtk_output_dir}"/"${gtdbtk_date_time}"_classification_all.tsv
+
+# CompareM is used to calculate the average amino acid identity (AAI) among the MAGs
+# comparem aai_wf <input_files> <output_dir>
+# comparem --cpus 32 aai_wf my_genomes aai_output
+echo "Running CompareM on dereplicated genomes (95% ANI threshold)"
+mkdir -p "${comparem_output_dir}"/"${comparem_date_time}"_aai_output_sa95perc
+comparem  aai_wf \
+ --cpus "${nthreads}" \
+ "${drep_output_dir}"/"${drep_date_time}"_sa_95perc/dereplicated_genomes \
+ -x fa \
+ "${comparem_output_dir}"/"${comparem_date_time}"_aai_output_sa95perc
+intermediate_date_time=$(date +"%F %H:%M:%S")
+echo "${intermediate_date_time}"
+
+echo "Running CompareM on dereplicated genomes (99% ANI threshold)"
+mkdir -p "${comparem_output_dir}"/"${comparem_date_time}"_aai_output_sa99perc
+comparem  aai_wf \
+ --cpus "${nthreads}" \
+ "${drep_output_dir}"/"${drep_date_time}"_sa_99perc/dereplicated_genomes \
+ -x fa \
+ "${comparem_output_dir}"/"${comparem_date_time}"_aai_output_sa99perc
+intermediate_date_time=$(date +"%F %H:%M:%S")
+echo "${intermediate_date_time}"
+
+
+# comparem_date_time
 
 
 # # 1347 MAGs in total
