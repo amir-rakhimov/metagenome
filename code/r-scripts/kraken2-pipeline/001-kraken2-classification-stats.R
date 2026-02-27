57cea32857132345a31767b0104bfdb8b6c2486b
amir-rakhimov <amir.rakhimov.b@gmail.com>
2026-02-27 18:14:31 +0900

    added comments

--- code/r-scripts/kraken2-pipeline/001-kraken2-classification-stats.R
+++ code/r-scripts/kraken2-pipeline/001-kraken2-classification-stats.R
@@ -1,6 +1,33 @@
-# Create a table that shows percentage of unclassified reads per sample
-# (**Table 5**).
-# The number of unclassified reads was taken from the job report.
+#' ---
+#' output: 
+#'   bookdown::html_document2:
+#'      toc: true
+#' ---
+#' 
+#' ```{r, setup 001-kraken2-classification-stats.R, include=FALSE}
+#' knitr::opts_knit$set(root.dir = '/home/rakhimov/projects/metagenome')
+#' ```
+#' ```{r, echo = FALSE}
+#' # For showing images, tables, etc: Use global path
+#' #knitr::spin("code/r-scripts/001-kraken2-classification-stats.R", knit = FALSE)
+#' #file.rename("code/r-scripts/001-kraken2-classification-stats.Rmd", 
+#'  "markdown/001-kraken2-classification-stats.Rmd")
+#' #rmarkdown::render('./markdown/001-kraken2-classification-stats.Rmd', 
+#'    'html_document',
+#' # knit_root_dir="/home/rakhimov/projects/metagenome/")
+#' ```
+#' 
+#+ echo=FALSE
+# Create a table with classification statistics ####
+#' # Create a table with classification statistics
+#' 
+#+ echo=FALSE
+## Introduction ####
+#'
+#' ## Introduction
+#' This script creates a table that shows percentage of unclassified reads 
+#' per sample. The number of unclassified reads was taken from the job report.
+# install.packages(c("tidyverse"))
 library(tidyverse)
 date_time<-"20240409_17_32_40"
 kraken2.results<-read.table(file.path("./output/kraken2_pipeline",
