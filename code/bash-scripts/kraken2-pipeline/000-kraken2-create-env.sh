69cc07c3ccea0188dfe1cb48660f1c0f6cb76484
amir-rakhimov <rakhimovamir2001@gmail.com>
2026-06-18 18:33:56 +0900

    moved SLURM parameters into a SLURM script

--- code/bash-scripts/kraken2-pipeline/000-kraken2-create-env.sh
+++ code/bash-scripts/kraken2-pipeline/000-kraken2-create-env.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+#SBATCH -t 24:00:00
+#SBATCH -J 20250916_00-59-kraken2-create-env
+#SBATCH --output jobreports/20250916_00-59-kraken2-create-env-out.txt
+#SBATCH --error jobreports/20250916_00-59-kraken2-create-env-out.txt
+
+#!/usr/bin/env bash
 source ~/miniconda3/etc/profile.d/conda.sh
 # Install all tools with conda
 ## Setup channels for conda installation
@@ -7,9 +13,11 @@ conda config --add channels bioconda
 conda config --add channels conda-forge
 
 # Delete the previous env
-conda remove --name kraken2-tools-2.1.3 --all
+# conda remove --name kraken2-tools-2.1.3 --all
 
 ## Create a conda environment
-conda create -yqn kraken2-tools-2.1.3 -c conda-forge -c bioconda kraken2 krakentools bracken krona bowtie2
+# conda create -yqn kraken2-tools-2.1.3 -c conda-forge -c bioconda kraken2 krakentools bracken krona bowtie2
+conda create -yqn kraken2-tools-2.1.6 -c conda-forge -c bioconda kraken2 krakentools bracken krona bowtie2
 
-conda activate kraken2-tools-2.1.3
+# conda activate kraken2-tools-2.1.3
+conda activate kraken2-tools-2.1.6
