d39e1704374dc9561f25a9f0a45950a613836c2f
amir-rakhimov <rakhimovamir2001@gmail.com>
2026-06-21 19:03:03 +0900

    removed unused scripts

--- code/shared/mpt-summary.R
+++
@@ -1,5 +0,0 @@
-args <- commandArgs(trailingOnly = TRUE)
-
-mpt.data<-read.table(args[1],header = F,sep = ",")
-summary(mpt.data$V2)
-print(c("Standard deviation:", round(sd(mpt.data$V2),digits = 2)))
