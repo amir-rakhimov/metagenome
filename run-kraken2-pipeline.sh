65f5bfccad6e03fa5f50acb494ab1d57f51de801
amir-rakhimov <rakhimovamir2001@gmail.com>
2026-06-21 17:34:55 +0900

    moved QC directories into a separate QC script

--- run-kraken2-pipeline.sh
+++ run-kraken2-pipeline.sh
@@ -2,29 +2,9 @@
 set -euo pipefail
 shopt -s nullglob
 kraken2_RUN_ID=$(date +%Y%m%d_%H%M%S)
-qc_RUN_ID=$(date +%Y%m%d_%H%M%S)
 # Load config
 source config/config.sh
 
-## data
-mkdir -p "${fastq_dir}"
-mkdir -p $HOME/common_data
-mkdir -p $HOME/common_data/reference_genomes
-
-# QC and decontamination
-mkdir -p "${fastqc_raw_output_dir}"
-mkdir -p "${multiqc_raw_output_dir}"
-mkdir -p "${fastqc_trimmed_output_dir}"
-mkdir -p "${multiqc_trimmed_output_dir}"
-mkdir -p "${cutadapt_output_dir}"
-
-mkdir -p "${bowtie2_sam_dir}"
-mkdir -p "${bowtie2_bam_dir}"
-mkdir -p "${bowtie2_filtered_bam_dir}"
-mkdir -p "${bowtie2_sorted_bam_dir}"
-mkdir -p "${fastqc_decontam_dir}"
-mkdir -p "${multiqc_decontam_dir}"
-mkdir -p "${bowtie2_decontam_fastq_dir}"
 
 # Kraken2
 mkdir -p "${kraken2_db_dir}"
@@ -40,8 +20,8 @@ mkdir -p "${bracken_krona_txt_dir}"
 mkdir -p "${krona_html_dir}"
 
 # mkdir -p results/1-kraken2/2-maaslin2
-job_id1=$(sbatch --parsable --export=ALL code/slurm-scripts/get-reference-data.slurm)
-job_id2=$(sbatch --parsable --dependency=afterok:$job_id1 --export=ALL code/slurm-scripts/get-toga-data.slurm)
+job_id1=$(sbatch --parsable --export=ALL code/slurm-scripts/001-kraken2-build.slurm)
+job_id2=$(sbatch --parsable --dependency=afterok:$job_id1 --export=ALL code/slurm-scripts/002-kraken2-pipeline.slurm)
 echo "Pipeline submitted"
 echo " step 1: ${job_id1}"
 echo " step 2: ${job_id2}"
